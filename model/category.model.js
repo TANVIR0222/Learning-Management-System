91c46c63c5bb357781de3c7e0f4c67e19f42ba60
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-03-02 19:37:40 +0600

    category model

---
+++ model/category.model.js
@@ -0,0 +1,16 @@
+import mongoose ,  {Schema} from 'mongoose'
+const categorySchema = new Schema({
+    title : {
+        require : true ,
+        type : String 
+    },
+    description : {
+        type : String 
+    },
+    thumbnail:{
+        require : true ,
+        type : String 
+    }
+})
+
+export const Category = mongoose.models.Category || mongoose.model("Category" , categorySchema)
